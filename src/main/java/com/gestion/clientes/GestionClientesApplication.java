79b4e516e298ef87ff9cf35f4e403cc7f8899eed
Misael <misaelcruz_983@hotmail.com>
2026-07-07 00:35:41 -0600

    Desarrollo del controlador con todos los metodos y pruebas iniciales con postman

--- src/main/java/com/gestion/clientes/GestionClientesApplication.java
+++ src/main/java/com/gestion/clientes/GestionClientesApplication.java
@@ -2,8 +2,14 @@ package com.gestion.clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.gestion")
+@EnableJpaRepositories(basePackages = "com.gestion.repositorio")
+@EntityScan(basePackages = "com.gestion.entidad")
 public class GestionClientesApplication {
 
 	public static void main(String[] args) {
